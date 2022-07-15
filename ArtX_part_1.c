a4030e574a4ad3edc4933c2531b3663e665eff17
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-15 21:45:58 +0400

    some editing

--- ArtX_part_1.c
+++ ArtX_part_1.c
@@ -23,7 +23,8 @@ void read_cb(struct ev_loop *loop, struct ev_io *watcher, int revents) {
   // читаем
   // чтобы работало на windows меняем ssize_t на int
   ssize_t read_count = recv(watcher->fd, buffer, 1024, MSG_NOSIGNAL);
-
+                              // MSG_NOSIGNAL - если соединение закрыто,
+                              // не генерировать сигнал SIG_PIPE
   // обрабатываем ошибку
   if (read_count < 0) { return; }
 
@@ -38,8 +39,12 @@ void read_cb(struct ev_loop *loop, struct ev_io *watcher, int revents) {
 
 void accept_cb(struct ev_loop *loop, struct ev_io *watcher, int revents) {
   // принимаем соединение
-  int client_socket = accept(watcher->fd, NULL, NULL);
-
+  int client_socket = accept(watcher->fd, NULL, 0);
+                                        // NULL - struct sockaddr *
+                                        // то есть ip-адрес и порт клиента
+                                        // 0 - размер этой структуры
+                                        // если указать NULL, 0 - игнорируем
+                                        // информацию о клиенте
   // watcher на чтение
   // выделяем память чтобы при выходе из call_back область видимости
   // структуры сохранилась в КУЧЕ
@@ -58,8 +63,8 @@ int main() {
   // создаем сокет: Internet-домен, TCP, протокол по умолчанию
   int sock;
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket!");
-    exit(1);
+    printf("socket_err!");
+    return -1;
   }
 
   // адрес
@@ -67,17 +72,24 @@ int main() {
   // занулим
   bzero(&address, sizeof(address));
   address.sin_family = AF_INET; // Internet-домен
-  address.sin_port = htons(port_number); // номер порта
-  address.sin_addr.s_addr = htonl(INADDR_ANY); // IP-адрес хоста
+  address.sin_port = htons(port_number); // номер порта (htons - приводим
+                                         // число с сетевому проядку байт short)
+  address.sin_addr.s_addr = htonl(INADDR_ANY); // IP-адрес хоста (htonl - приводим
+                                            // число к сетевому порядку байт long)
+                                            // INADDR_ANY              0.0.0.0
+                                            // INADDR_LOOPBACK       127.0.0.1
+  // чтобы указать конкретный ip-адрес используется ip = inet_addr("10.0.0.1");
+  // или новая ip = inet_pton(AF_INET, "10.0.0.1", &(address.sin_addr);
 
   // свяжем сокет с адресом
-  if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror ("bind!");
-    exit(2);
+  if (bind(sock, (struct sockaddr *)(&address), sizeof(address)) < 0) {
+    printf("bind_err!");
+    return -2;
   }
 
   // открываем слушающий TCP сокет
-  listen(sock, SOMAXCONN);
+  listen(sock, SOMAXCONN);  // длина очереди ожидания: 128 - для linux < 5.4
+                            //                        4096 - для linux >= 5.4
 
   // инициализируем watcher
   struct ev_io w_accept;
